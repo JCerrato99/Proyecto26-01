4ac2f47f84ea0b759a49085548aebe784e2ad5c6
Alumno <Alumno@2DAM01>
2024-01-26 18:12:18 +0100

    Segundo commit

---
+++ WindowInput.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+
+namespace Sergioteacher.Csharp05.EditorTextoA
+{
+    /// <summary>
+    /// Lógica de interacción para WindowInput.xaml
+    /// </summary>
+    public partial class WindowInput : Window
+    {
+        MainWindow Inicio;
+        public WindowInput(MainWindow ventanaInicio)
+        {
+            Inicio = ventanaInicio;
+            InitializeComponent();
+
+        }
+
+        private void BIntro_Click(object sender, RoutedEventArgs e)
+        {
+            Inicio.data = MiTexto.Text;
+            Inicio.Show();
+            this.Hide();
+        }
+
+        private void VentanaInput_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.Visibility == Visibility.Visible)
+            {
+                //MessageBox.Show("Hola, mensaje al capturar ->  X");
+                e.Cancel = true;
+                Inicio.data = "";
+                Inicio.Show();
+                this.Hide();
+            }
+        }
+
+        private void VentanaInput_Activated(object sender, EventArgs e)
+        {
+            MiTexto.Text = "";
+        }
+    }
+}
